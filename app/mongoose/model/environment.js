2149799de020887ef94269472f65b11a2a3ca295
PaulWassermann <58108761+PaulWassermann@users.noreply.github.com>
2021-06-10 15:11:27 +0200

    Delete app directory

--- app/mongoose/model/environment.js
+++
@@ -1,4 +0,0 @@
-import mongoose from 'mongoose'
-import environSchema from '../schemas/environment'
-
-module.exports = mongoose.model('Video',environSchema);
--- app/mongoose/model/video.js
+++
@@ -1,4 +0,0 @@
-import mongoose from 'mongoose'
-import videoSchema from '../schemas/video'
-
-module.exports = mongoose.model('Video',videoSchema);
